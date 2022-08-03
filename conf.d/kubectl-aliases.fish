adc39f281f20e86a7c99e9e67b1cdfce0bf680e9
mib <mib@kanp.ai>
2022-08-03 02:53:12 +0200

    clunky fisher support

---
+++ conf.d/kubectl-aliases.fish
@@ -0,0 +1,7 @@
+function _kubectl-aliases_generate --on-event kubectl-aliases_install
+    set fish_dir "$HOME/.config/fish"
+    set abbr_file "$fish_dir/completions/kubectl_aliases.fish"
+    set abbreviations "$(python ~/.config/fish/conf.d/kubectl-aliases-generate.py fish)"
+    echo "$abbreviations" > $abbr_file
+    source $abbr_dir
+end
